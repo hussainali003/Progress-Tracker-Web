89a2de6223e83b8f2af64c59729ccde318d81e09
hussainali003 <hussainali9203@gmail.com>
2025-12-12 15:47:25 +0500

    feat: connect login page to backend

--- src/api/auth.js
+++ src/api/auth.js
@@ -15,3 +15,19 @@ export async function registerUser({ name, email, password }) {
 
   return res.json(); // { name, id }
 }
+
+export async function loginUser({ email, password }) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  // If backend throws 422 (email taken)
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "Registration failed.");
+  }
+
+  return res.json(); // { name, id, token }
+}
